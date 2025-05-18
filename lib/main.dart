9d968f2524507147e0ccb856bdfafaaa9e399277
Kavindu Jeewantha <lasithakavindu02@gmail.com>
2025-05-18 21:35:37 +0530

    updated!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:simplechatapp/common/pages.dart';
 import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +15,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return GetMaterialApp(
+      title:"Chat App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      initialRoute: AppPages.INITIAL,
+    );
   }
 }
